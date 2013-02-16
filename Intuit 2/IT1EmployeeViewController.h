726f323eaec9b90247430b4aa3804ddeccda0428
Rupert Deese <rdeese@hmc.edu>
2013-02-16 15:43:42 -0800

    Commit with autorotating employee view in progress

---
+++ Intuit 2/IT1EmployeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  IT1EmployeeViewController.h
+//  Intuit 2
+//
+//  Created by Rupert Deese on 2/12/13.
+//  Copyright (c) 2013 Rupert Deese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IT1EmployeeViewController : UIViewController
+
+@end
---
+++ Intuit 2/IT1EmployeeViewController.m
@@ -0,0 +1,38 @@
+//
+//  IT1EmployeeViewController.m
+//  Intuit 2
+//
+//  Created by Rupert Deese on 2/12/13.
+//  Copyright (c) 2013 Rupert Deese. All rights reserved.
+//
+
+#import "IT1EmployeeViewController.h"
+
+@interface IT1EmployeeViewController ()
+
+@end
+
+@implementation IT1EmployeeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
